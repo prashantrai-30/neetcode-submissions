a74afed8722871c8ceea267cc482c2b8034a1ddd
Prashant Rai <prashant.2023ug1115@iiitranchi.ac.in>
2026-06-08 20:25:11 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+private:
+    bool isPalindrome(int l,int r,string s) {
+        while(l < r) {
+            if(s[l] != s[r]) return false; 
+            l++;
+            r--;
+        }
+        return true;
+    }
+public:
+    bool validPalindrome(string s) {
+        int l = 0,r = s.size() - 1;
+        while(l < r) {
+            if(s[l] != s[r]) {
+                return (isPalindrome(l+1,r,s) || isPalindrome(l,r-1,s));
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
