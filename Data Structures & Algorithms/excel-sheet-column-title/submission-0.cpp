f2ec2113ef7c498610e27b294c293fd7036982ea
Prashant Rai <prashant.2023ug1115@iiitranchi.ac.in>
2026-06-25 10:03:21 +0530

    Add: excel-sheet-column-title - submission-0

---
+++ Data Structures & Algorithms/excel-sheet-column-title/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        int n = columnNumber;
+        string res = "";
+        while(n > 0) {
+            n--;
+            res+=('A' + n%26);
+            n = n/26;
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
